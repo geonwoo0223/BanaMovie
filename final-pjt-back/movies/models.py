40c7fd495bf8a4f63c4ea56b8b297299e6c13d2c
geonwoo0223 <geonwoo0223@gmail.com>
2020-11-19 14:09:25 +0900

    스켈레톤+ Movie Model

---
+++ final-pjt-back/movies/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Genre(models.Model):
+  name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+  title = models.CharField(max_length=100)
+  release_date = models.DateField()
+  poster_path = models.CharField(max_length=200)
+  adult = models.BooleanField(default=False)
+  overview = models.TextField()
+  vote_count = models.IntegerField(default=0)
+  rate = models.FloatField(default=0.0)
+  genres = models.ManyToManyField(Genre)
+  status = models.BooleanField(default=False)
+  admin_reg = models.BooleanField(default=False)
