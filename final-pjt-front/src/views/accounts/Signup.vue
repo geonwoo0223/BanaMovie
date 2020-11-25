92bacb6ac22ccb51b1306307e04128503fee2ff9
geonwoo0223 <geonwoo0223@gmail.com>
2020-11-25 14:10:55 +0900

    회원가입 끝

--- final-pjt-front/src/views/accounts/Signup.vue
+++ final-pjt-front/src/views/accounts/Signup.vue
@@ -3,15 +3,16 @@
     
     <h1 class="font-do my-3">Sign up</h1>
   
-      <div class="form-group mt-5 font-poor font-1-2em">
+    <div class="form-group mt-5 font-poor font-1-2em">
       <label for="username" >사용자 이름: </label>   
       <input type="text" class="form-control" id="username" v-model="credentials.username">
-      </div>
+    </div>
 
     <div class="form-group mt-5 font-poor font-1-2em">
       <label for="password">비밀번호: </label>
       <input type="password" class="form-control" id="password" v-model="credentials.password">
     </div>
+
     <div class="form-group mt-5 font-poor font-1-2em">
       <label for="passwordConfirmation">비밀번호 확인: </label>
       <input 
@@ -22,11 +23,11 @@
         class="form-control"
       >
     </div>
-  <div class="form-group mt-5">
+
+    <div class="form-group mt-5">
       <label for="email">이메일: </label>
       <input type="email" class="form-control" id="email" v-model="credentials.email">
        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-
     </div>
 
     <div class="form-group mt-5 font-poor font-1-2em">
@@ -35,17 +36,19 @@
     </div>
 
     <div class="form-group mt-3 float-left">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck" v-model="agree">
-      <label class="form-check-label" for="gridCheck">
-        개인정보 이용 약관에 동의합니다.
-      </label>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck" checked="true" v-model="agree">
+        <label class="form-check-label" for="gridCheck">
+          <p @click="agreePage">개인정보 이용 약관에 동의합니다.</p>
+        </label>
+      </div>
     </div>
-  </div>
   <br>
 
+    <div class="form-group mt-5 font-poor font-1-2em">
+      <button @click="signup" id="signupbtn" class="btn btn-pink my-5 form-group">회원가입</button>
+    </div>
 
-    <button @click="signup" id="signupbtn" class="btn btn-pink my-5">회원가입</button>
   </div>
 </template>
 
@@ -63,9 +66,8 @@ export default {
         password: '',
         email: '',
         date_of_birth: '',
-        agree: false,
-
-      }
+      },
+      agree: false,
     }
   },
   methods: {
@@ -75,6 +77,7 @@ export default {
           .then((res) => {
             console.log(res)
             this.$router.push({ name: 'Login' })
+            this.agree = false
           })
           .catch((err) => {
             console.log(err)
@@ -83,6 +86,9 @@ export default {
       } else {
         alert("개인정보 이용 약관에 동의해야합니다.")
       }
+    },
+    agreePage: function () {
+      alert("① 정보주체는 DeJaVue에 대해 언제든지 개인정보 열람,정정,삭제,처리정지 요구 등의 권리를 행사할 수 있습니다. \n② 제1항에 따른 권리 행사는DeJaVue에 대해 개인정보 보호법 시행령 제41조제1항에 따라 서면, 전자우편, 모사전송(FAX) 등을 통하여 하실 수 있으며 DeJaVue은(는) 이에 대해 지체 없이 조치하겠습니다. \n③ 제1항에 따른 권리 행사는 정보주체의 법정대리인이나 위임을 받은 자 등 대리인을 통하여 하실 수 있습니다. 이 경우 개인정보 보호법 시행규칙 별지 제11호 서식에 따른 위임장을 제출하셔야 합니다. \n④ 개인정보 열람 및 처리정지 요구는 개인정보보호법 제35조 제5항, 제37조 제2항에 의하여 정보주체의 권리가 제한 될 수 있습니다. \n⑤ 개인정보의 정정 및 삭제 요구는 다른 법령에서 그 개인정보가 수집 대상으로 명시되어 있는 경우에는 그 삭제를 요구할 수 없습니다. \n⑥ DeJaVue은(는) 정보주체 권리에 따른 열람의 요구, 정정·삭제의 요구, 처리정지의 요구 시 열람 등 요구를 한 자가 본인이거나 정당한 대리인인지를 확인합니다.")
     }
   }
 }
