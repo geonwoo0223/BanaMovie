05eb04df05c3cffed86f72bd6d51a81ced0c5fdb
김민정 <snowcuphea@gmail.com>
2020-11-26 17:01:18 +0900

    유저 삭제 시 게시글 처리

--- final-pjt-back/community/models.py
+++ final-pjt-back/community/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
+
+
+def set_FK_Model_user():
+    return get_user_model().objects.get(username="삭제된유저")
 
 # Create your models here.
 class Board(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(set_FK_Model_user))
     title = models.CharField(max_length=200)
     content = models.TextField()
     board_code = models.IntegerField()
@@ -16,7 +21,7 @@ class Board(models.Model):
 
 
 class BoardComment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(set_FK_Model_user))
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
